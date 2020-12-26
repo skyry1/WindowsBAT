9966f31fd7659ed4e2da4852a10aa0077ef5b5fb
skyry1 <skyry1@icloud.com>
2020-12-26 16:51:08 +0900

    テストファイル作成用BATを追加

---
+++ CreateTestFiles.bat
@@ -0,0 +1,33 @@
+@echo off
+echo echo 〇〇システムA機能.bat >> 〇〇システムA機能.bat
+echo echo 〇〇システムB機能.bat >> 〇〇システムB機能.bat
+echo echo 〇〇システムC機能.bat >> 〇〇システムC機能.bat
+echo echo 〇〇システムD機能.bat >> 〇〇システムD機能.bat
+echo echo 〇〇システムE機能.bat >> 〇〇システムE機能.bat
+echo echo 〇〇システムF機能.bat >> 〇〇システムF機能.bat
+echo echo 〇〇システムG機能.bat >> 〇〇システムG機能.bat
+echo echo 〇〇システムH機能.bat >> 〇〇システムH機能.bat
+echo echo 〇〇システムI機能.bat >> 〇〇システムI機能.bat
+echo echo 〇〇システムJ機能.bat >> 〇〇システムJ機能.bat
+echo echo 〇〇システムK機能.bat >> 〇〇システムK機能.bat
+echo echo 〇〇システムL機能.bat >> 〇〇システムL機能.bat
+echo echo 〇〇システムM機能.bat >> 〇〇システムM機能.bat
+echo echo 〇〇システムN機能.bat >> 〇〇システムN機能.bat
+echo echo 〇〇システムO機能.bat >> 〇〇システムO機能.bat
+echo echo 〇〇システムP機能.cmd >> 〇〇システムP機能.cmd
+echo pause >> 〇〇システムA機能.bat
+echo pause >> 〇〇システムB機能.bat
+echo pause >> 〇〇システムC機能.bat
+echo pause >> 〇〇システムD機能.bat
+echo pause >> 〇〇システムE機能.bat
+echo pause >> 〇〇システムF機能.bat
+echo pause >> 〇〇システムG機能.bat
+echo pause >> 〇〇システムH機能.bat
+echo pause >> 〇〇システムI機能.bat
+echo pause >> 〇〇システムJ機能.bat
+echo pause >> 〇〇システムK機能.bat
+echo pause >> 〇〇システムL機能.bat
+echo pause >> 〇〇システムM機能.bat
+echo pause >> 〇〇システムN機能.bat
+echo pause >> 〇〇システムO機能.bat
+echo pause >> 〇〇システムP機能.cmd
